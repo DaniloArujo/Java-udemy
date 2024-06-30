493bc39b2fa9e5e168f5307a011f4bc22e1f4167
danilo <daniloaraujomota@gmail.com>
2024-06-30 19:31:42 -0300

    .

--- JavaUdemy/src/Main.java
+++ JavaUdemy/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.err.println("Ola, mundo!");
+    public static void main(String[] args){
+        String nome = "Danilo";
+        int idade = 29;
+        double renda = 1200.00;
+        System.out.printf("%s tem %d anos, e ganha R$ %.2f reais\n", nome, idade, renda);
+        
     }
 }
