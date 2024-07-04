857c610ae1e825ae2a19288da5e994eaf9364561
danilo <daniloaraujomota@gmail.com>
2024-07-04 12:40:06 -0300

    .

---
+++ JavaUdemy/src/Ex001.java
@@ -0,0 +1,21 @@
+public class Ex001 {
+    public static void main(String[] args){
+        
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.00;
+        double price2 = 650.50;
+        double measure = 53.234567;
+        
+        System.out.println("Products: ");
+        System.out.printf("%s, wich the price is %f\n",product1,price1);
+        System.out.printf("%s, wich the price is %f\n", product2,price2);
+        
+
+    }
+}
