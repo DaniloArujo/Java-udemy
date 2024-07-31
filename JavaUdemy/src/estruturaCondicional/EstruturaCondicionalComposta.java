7cfceaf11e642d2708da4c7ac99fbf5cf89cd25c
danilo <daniloaraujomota@gmail.com>
2024-07-31 11:11:03 -0300

    .

--- JavaUdemy/src/estruturaCondicional/EstruturaCondicionalComposta.java
+++ JavaUdemy/src/estruturaCondicional/EstruturaCondicionalComposta.java
@@ -2,10 +2,12 @@ package estruturaCondicional;
 
 public class EstruturaCondicionalComposta {
     public static void main(String[] args) {
-        String periodo = "tarde";
+        int periodo = 14;
 
-        if (periodo == "manha"){
+        if (periodo < 12){
             System.out.println("Bom dia!");
+        }else if (periodo < 18) {
+            System.out.println("Boa tarde!");
         }else{
             System.out.println("Boa noite!");
         }
