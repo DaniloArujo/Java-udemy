72c9317bd41861eb395a641cbed5859135698bfd
danilo <daniloaraujomota@gmail.com>
2024-07-31 10:46:23 -0300

    .

---
+++ JavaUdemy/src/estruturaCondicional/EstruturaCondicional.java
@@ -0,0 +1,19 @@
+package estruturaCondicional;
+
+public class EstruturaCondicional{
+    public static void main(String[] args){
+        int x = 5;
+
+        if(x < 5){
+            System.out.println("Bom dia!");
+        }
+
+        if(x == 5){
+            System.out.println("Boa tarde!");
+        }
+
+        if(x > 5){
+            System.out.println("Boa noite!");
+        }
+    }
+}
