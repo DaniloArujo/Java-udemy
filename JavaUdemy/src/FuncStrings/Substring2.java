fc574610feb5d5df854ebca0677974f37a4b5a35
danilo <daniloaraujomota@gmail.com>
2024-08-13 12:34:10 -0300

    .

---
+++ JavaUdemy/src/FuncStrings/Substring2.java
@@ -0,0 +1,14 @@
+package FuncStrings;
+
+public class Substring2 {
+    public static void main(String[] args) {
+        String original = "abcde EFGHI ABC abc DEFG      ";
+        String sub = original.substring(2, 9);
+
+        System.out.println("#####################################");
+        System.out.println(original);
+        System.out.println(sub);
+
+
+    }   
+}
