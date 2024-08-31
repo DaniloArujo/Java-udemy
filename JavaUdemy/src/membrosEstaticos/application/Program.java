dd0b1236d933f07e42fcb949ff732b785dfd0176
danilo <daniloaraujomota@gmail.com>
2024-08-30 22:31:05 -0300

    .

--- JavaUdemy/src/membrosEstaticos/application/Program.java
+++ JavaUdemy/src/membrosEstaticos/application/Program.java
@@ -10,18 +10,17 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
-        Calculator calc = new Calculator();
 
         System.out.print("Enter radius: ");
         double radius = scanner.nextDouble();
 
-        double c = calc.circumference(radius);
+        double c = Calculator.circumference(radius);
 
-        double v = calc.volume(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f\n", c);
         System.out.printf("Volume: %.2f\n", v);
-        System.out.printf("Pi value: %.2f\n", calc.PI);
+        System.out.printf("Pi value: %.2f\n", Calculator.PI);
 
         scanner.close();
     }
--- JavaUdemy/src/membrosEstaticos/util/Calculator.java
+++ JavaUdemy/src/membrosEstaticos/util/Calculator.java
@@ -2,13 +2,13 @@ package membrosEstaticos.util;
 
 public class Calculator {
     
-    public final double PI = 3.14159;
+    public static final double PI = 3.14159;
     
-    public double circumference(double radius){
+    public static double circumference(double radius){
         return 2.00 * radius * PI;
     }
 
-    public double volume(double radius){
+    public static double volume(double radius){
         return 4 * PI * (Math.pow(radius, 3)) / 3.0;
     }
 }
