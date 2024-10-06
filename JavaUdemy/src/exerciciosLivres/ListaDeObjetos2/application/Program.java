6dea1e3da89b3baeff1670e60759d489e79ca357
danilo <daniloaraujomota@gmail.com>
2024-10-06 07:08:14 -0300

    .

--- JavaUdemy/src/exerciciosLivres/ListaDeObjetos2/application/Program.java
+++
@@ -1,5 +0,0 @@
-package exerciciosLivres.ListaDeObjetos2.application;
-
-public class Program {
-    
-}
---
+++ JavaUdemy/src/exerciciosLivres/exercicioListas/Program.java
@@ -0,0 +1,24 @@
+package exerciciosLivres.exercicioListas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Danilo");
+        list.add("Alex");
+        list.add("Matheus");
+        list.add("Nicholas");
+        list.add("Bob");
+        list.add("Anna");
+
+        for(String x : list){
+            System.out.println(x);
+        }
+
+        
+    }
+}
