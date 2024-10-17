2e92323c5ae438f980c34181e2082a1d26bcb9e9
danilo <daniloaraujomota@gmail.com>
2024-10-17 10:42:53 -0300

    .

---
+++ JavaUdemy/src/Heranca/application/Application.java
@@ -0,0 +1,13 @@
+package Heranca.application;
+
+import exerciciosLivres.heranca01.entities.BusinessAccount;
+
+public class Application {
+    public static void main(String[] args) {
+
+        BusinessAccount b1 = new BusinessAccount(1,"Danilo",4000.00,4000.00);
+
+        System.out.println(b1);
+
+    }
+}
---
+++ JavaUdemy/src/exerciciosLivres/heranca01/entities/BusinessAccount.java
@@ -0,0 +1,27 @@
+package exerciciosLivres.heranca01.entities;
+
+public class BusinessAccount extends Account{
+
+    Double LoanLimit;
+
+    public BusinessAccount() {
+        super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        LoanLimit = loanLimit;
+    }
+
+    public void loan(Double amount){
+        balance += amount;
+    }
+
+    @Override
+    public String toString() {
+        return "BusinessAccount{" +
+                "LoanLimit=" + LoanLimit +
+                ", balance=" + balance +
+                "} " + super.toString();
+    }
+}
